0f72606699482f6a1e5bdfd78a9d80184eed71f7
jamie ontiveros <jamieontiveros@Jamies-MacBook-Pro.local>
2015-06-21 18:46:40 -0700

    ready to push

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MAIN_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'coffeedapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,10 +115,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
-    os.path.join(MAIN_DIR, 'templates'),
+    os.path.join(BASE_DIR, 'templates'),
     )
 
 STATICFILES_DIRS = (
-    os.path.join(MAIN_DIR, 'static'))
-
+    os.path.join(BASE_DIR, 'static'),
+    )
 
